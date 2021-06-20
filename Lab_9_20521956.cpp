45ca62da10129c9159c4b0dc23b60021e3b54872
Admin <tuivanthiet@gmail.com>
2021-06-20 22:29:05 +0700

    fix hash key

--- Lab_9_20521956.cpp
+++ Lab_9_20521956.cpp
@@ -16,17 +16,19 @@ void init(node* hashtable[]);
 void destroy(node* hashtable[]);
 int F1(int key);
 int F2(int key);
-bool search_linear(node* hashtable[], int key);
-bool search_quad(node* hashtable[], int key);
-void insertHash_linear(node* hashtable[], int key);
-void insertHash_quad(node* hashtable[], int key);
-void insertHash_linked(node* hashtable[], int key);
+bool search_linear(node* hashtable[], int(*function)(int key), int key);
+bool search_quad(node* hashtable[], int(*function)(int key), int key);
+bool search_linked(node* hashtable[], int(*function)(int key), int key);
+void insertHash_linear(node* hashtable[], int(*function)(int key), int key);
+void insertHash_quad(node* hashtable[], int(*function)(int key), int key);
+void insertHash_linked(node* hashtable[], int(*function)(int key), int key);
 void print(node* hashtable[]);
 int menu();
+int hash_menu();
 bool exec(node* hashtable[], int arr[], int n, int mode = menu());
 
 int main() {
-	int arr[] = { 32,15,25,44,36,21 };
+	int arr[] = { 32,15,25,44,36,21,15 };
 	int n = sizeof(arr) / sizeof(int);
 	node* hashtable[M];
 	init(hashtable);
@@ -55,8 +57,8 @@ int F1(int key) {
 int F2(int key) {
 	return (M - 2) - key % (M - 2);
 }
-bool search_linear(node* hashtable[], int key) {
-	int index = F1(key);
+bool search_linear(node* hashtable[], int(*function)(int key), int key) {
+	int index = (*function)(key);
 	while (hashtable[index] != NULL
 		&& hashtable[index]->data != key) {
 		index = (index + 1) % M;
@@ -64,39 +66,49 @@ bool search_linear(node* hashtable[], int key) {
 	if (hashtable[index] == NULL)return false;
 	return true;
 }
-bool search_quad(node* hashtable[], int key) {
-	int index = F1(key);
+bool search_quad(node* hashtable[], int(*function)(int key), int key) {
+	int index = (*function)(key);
 	int i = 1;
 	while (hashtable[index] != NULL
 		&& hashtable[index]->data != key) {
-		index = (F1(key) + i * i) % M;
+		index = ((*function)(key) + i * i) % M;
 		i++;
 	}
 	if (hashtable[index] == NULL)return false;
 	return true;
 }
-void insertHash_linear(node* hashtable[], int key) {
-	if (search_linear(hashtable, key))return;
-	int index = F1(key);
+bool search_linked(node* hashtable[], int(*function)(int key), int key) {
+	node* p = hashtable[(*function)(key)];
+	while (p != NULL
+		&& p->data != key) {
+		p = p->next;
+	}
+	if (p == NULL)return false;
+	return true;
+}
+void insertHash_linear(node* hashtable[], int(*function)(int key), int key) {
+	if (search_linear(hashtable, (*function), key))return;
+	int index = (*function)(key);
 	while (hashtable[index] != NULL) {
 		index = (index + 1) % M;
 	}
 	hashtable[index] = new node(key);
 	N++;
 }
-void insertHash_quad(node* hashtable[], int key) {
-	if (search_quad(hashtable, key) || N >= M - 1)return;
+void insertHash_quad(node* hashtable[], int(*function)(int key), int key) {
+	if (search_quad(hashtable, (*function), key) || N >= M - 1)return;
 	int index = F1(key);
 	int i = 1;
 	while (hashtable[index] != NULL) {
-		index = (F1(key) + i * i) % M;
+		index = ((*function)(key) + i * i) % M;
 		i++;
 	}
 	hashtable[index] = new node(key);
 	N++;
 }
-void insertHash_linked(node* hashtable[], int key) {
-	int index = F1(key);
+void insertHash_linked(node* hashtable[], int(*function)(int key), int key) {
+	if (search_linked(hashtable, (*function), key))return;
+	int index = (*function)(key);
 	node* p = hashtable[index];
 	if (p == NULL) {
 		hashtable[index] = new node(key);
@@ -141,15 +153,32 @@ int menu() {
 	} while (choose < 1 || choose>4);
 	return choose;
 }
+int hash_menu() {
+	int choose;
+	do {
+		::system("cls");
+		cout << "1. Dung ham bam: F1(key) = key % M.\n";
+		cout << "2. Dung ham bam: F2(key) = (M - 2) - key % (M - 2).\n";
+		cout << "Lua chon cua ban: \n";
+		cin >> choose;
+		if (choose < 1 || choose>2) {
+			cout << "Khong hop le, nhap lai.\n";
+			::system("pause");
+		}
+	} while (choose < 1 || choose>2);
+	return choose;
+}
 bool exec(node* hashtable[], int arr[], int n, int mode) {
 	::system("cls");
+	int hash = 0;
+	if (mode != 4) hash = hash_menu();
 	switch (mode)
 	{
 	case 1:
 		destroy(hashtable);
 		init(hashtable);
 		for (int i = 0; i < n; i++) {
-			insertHash_linear(hashtable, arr[i]);
+			insertHash_linear(hashtable, hash == 1 ? F1 : F2, arr[i]);
 		}
 		print(hashtable);
 		break;
@@ -157,7 +186,7 @@ bool exec(node* hashtable[], int arr[], int n, int mode) {
 		destroy(hashtable);
 		init(hashtable);
 		for (int i = 0; i < n; i++) {
-			insertHash_quad(hashtable, arr[i]);
+			insertHash_quad(hashtable, hash == 1 ? F1 : F2, arr[i]);
 		}
 		print(hashtable);
 		break;
@@ -165,7 +194,7 @@ bool exec(node* hashtable[], int arr[], int n, int mode) {
 		destroy(hashtable);
 		init(hashtable);
 		for (int i = 0; i < n; i++) {
-			insertHash_linked(hashtable, arr[i]);
+			insertHash_linked(hashtable, hash == 1 ? F1 : F2, arr[i]);
 		}
 		print(hashtable);
 		break;
